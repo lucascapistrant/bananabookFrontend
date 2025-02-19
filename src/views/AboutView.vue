a492e78b72928b68a1b45eef3baee35630547ce7
Lucas Capistrant <lucas.o.capistrant@gmail.com>
2025-02-18 20:59:17 -0600

    Setup - Vue project + router setup

---
+++ src/views/AboutView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>About page</h1>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+// import component from '../components/component.vue'
+</script>
+
+<template>
+  <main>
+    <h1>Home page</h1>
+    <!-- <component/> -->
+  </main>
+</template>
