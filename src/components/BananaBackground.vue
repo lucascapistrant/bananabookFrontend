24619cc1de0a48c6c9a23708bd9fd05c7419800b
Lucas Capistrant <lucas.o.capistrant@gmail.com>
2025-02-24 21:26:22 -0600

    FEAT: Repeating Banana background component

---
+++ src/components/BananaBackground.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="background"></div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+    .background {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-image: url(../assets/images/Banana.svg);
+        background-size: 25%;
+        background-repeat: repeat;
+        background-color: var(--color-background);
+        z-index: -10;
+    }
+</style>
