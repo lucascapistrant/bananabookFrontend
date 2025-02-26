d7c1eb798dbc2e6e127978988bc09d71ca4525c8
Lucas Capistrant <lucas.o.capistrant@gmail.com>
2025-02-25 19:45:34 -0600

    FEAT: Authentication box (login, register buttons) component added

---
+++ src/components/AuthenticationBox.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Button from './Button.vue';
+</script>
+
+<template>
+  <div class="container">
+    <Button type="primary" block >login</Button>
+    <Button type="secondary" block >Register</Button>
+  </div>
+</template>
+
+<style scoped>
+.container {
+    background: var(--color-container);
+    padding: 3rem;
+    border-radius: 30px;
+    max-width: 300px;
+    box-shadow: 4px 4px 4px #777;
+}
+
+Button:first-of-type {
+    margin-bottom: 20px;
+}
+</style>
