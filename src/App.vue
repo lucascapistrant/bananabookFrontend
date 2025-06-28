96a0dd2f2af1c9d31eab60fb9c8ff31e66776279
Lucas Capistrant <lucas.o.capistrant@gmail.com>
2025-06-27 19:19:21 -0500

    DEV: Added a temporary button at the top of all pages that links to login page

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import Button from './components/Button.vue';
 </script>
 
 <template>
-
+  <router-link :to="`/login`">
+    <Button color="primary">Login Page</Button>
+  </router-link>
   <RouterView />
 </template>
 
